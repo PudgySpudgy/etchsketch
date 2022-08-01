1febf4c4d33c8cf3e8add5b1d3ef25dc59202ac3
NotSoPudgySpudgy <phillips0517@protonmail.com>
2022-08-01 18:14:07 -0400

    4x4 grid created in js, basic color in css

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Initial iteration of Project: Etch-a-Sketch.
 
 Creation of index.html, with Emmet boilerplate.
 Creation of script.js.
+
+To Do list
+
+create and append container holding 16 divs
+create and append 16 divs
---
+++ script.js
@@ -0,0 +1,69 @@
+const container = document.getElementById('container');
+const divOne = document.createElement('div');
+const divTwo = document.createElement('div');
+const divThree = document.createElement('div');
+const divFour = document.createElement('div');
+const divFive = document.createElement('div');
+const divSix = document.createElement('div');
+const divSeven = document.createElement('div');
+const divEight = document.createElement('div');
+const divNine = document.createElement('div');
+const divTen = document.createElement('div');
+const divEleven = document.createElement('div');
+const divTwelve = document.createElement('div');
+const divThirteen = document.createElement('div');
+const divFourteen = document.createElement('div');
+const divFifteen = document.createElement('div');
+const divSixteen = document.createElement('div');
+
+divOne.innerText = '1'
+divTwo.innerText = '2'
+divThree.innerText = '3'
+divFour.innerText = '4'
+divFive.innerText = '5'
+divSix.innerText = '6'
+divSeven.innerText = '7'
+divEight.innerText = '8'
+divNine.innerText = '9'
+divTen.innerText = '10'
+divEleven.innerText = '11'
+divTwelve.innerText = '12'
+divThirteen.innerText = '13'
+divFourteen.innerText = '14'
+divFifteen.innerText = '15'
+divSixteen.innerText = '16'
+
+divOne.className = 'box';
+divTwo.className = 'box';
+divThree.className = 'box';
+divFour.className = 'box';
+divFive.className = 'box';
+divSix.className = 'box';
+divSeven.className = 'box';
+divEight.className = 'box';
+divNine.className = 'box';
+divTen.className = 'box';
+divEleven.className = 'box';
+divTwelve.className = 'box';
+divThirteen.className = 'box';
+divFourteen.className = 'box';
+divFifteen.className = 'box';
+divSixteen.className = 'box';
+
+container.appendChild(divOne);
+container.appendChild(divTwo);
+container.appendChild(divThree);
+container.appendChild(divFour);
+container.appendChild(divFive);
+container.appendChild(divSix);
+container.appendChild(divSeven);20%
+container.appendChild(divEight);
+container.appendChild(divNine);
+container.appendChild(divTen);
+container.appendChild(divEleven);
+container.appendChild(divTwelve);
+container.appendChild(divThirteen);
+container.appendChild(divFourteen);
+container.appendChild(divFifteen);
+container.appendChild(divSixteen);
+
